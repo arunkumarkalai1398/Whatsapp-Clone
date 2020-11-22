5fa2dfe22bab00547ea3ee9cbc554b88af419f52
arunkumarkalai1398 <57340179+arunkumarkalai1398@users.noreply.github.com>
2020-11-22 12:42:27 +0530

    Add files via upload

---
+++ build/precache-manifest.e47a8ff47bfbf2880cdeb8b75912de82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f23bc43bbf8a4c7363124a353b13f11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "719d017019afa0858800",
+    "url": "/static/css/2.52b32611.chunk.css"
+  },
+  {
+    "revision": "4f871fc0386d3baed1b3",
+    "url": "/static/css/main.31948d6f.chunk.css"
+  },
+  {
+    "revision": "719d017019afa0858800",
+    "url": "/static/js/2.bf20fe79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf20fe79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f871fc0386d3baed1b3",
+    "url": "/static/js/main.25282cca.chunk.js"
+  },
+  {
+    "revision": "1422f143402fd369506c",
+    "url": "/static/js/runtime-main.c4c9f97b.js"
+  }
+]);
